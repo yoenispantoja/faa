773a486b5ab74a59c8d38aa24d2ef9502f0fddef
ypantojaz <ypantojaz@uci.cu>
2019-02-28 02:45:15 -0500

    Modal with datalist

--- src/app/layout/indisciplinas/modals/add-indisciplina/add-indisciplina.component.ts
+++ src/app/layout/indisciplinas/modals/add-indisciplina/add-indisciplina.component.ts
@@ -5,7 +5,10 @@ import { IndisciplinasService } from '../../../../shared/services/indisciplinas.
 import { SweetAlert2Module, SwalComponent } from '@toverux/ngx-sweetalert2'; //para los sweetAlerts
 import { Router, NavigationEnd } from '@angular/router';
 import { CategoriasService } from 'src/app/shared/services/categorias.service';
+import { DemandantesService } from 'src/app/shared/services/demandantes.service';
+
 import {Implicado} from '../../implicado';
+import { SancionadosService } from 'src/app/shared/services/sancionados.service';
 
 @Component({
   selector: 'app-add-indisciplina',
@@ -15,6 +18,7 @@ import {Implicado} from '../../implicado';
 export class AddIndisciplinaComponent implements OnInit {
   //Atributos
   @ViewChild('confirmSwal') private confirmSwal: SwalComponent;
+  @ViewChild('existeSancionadoSwal') private existeSancionadoSwal: SwalComponent;
   @Input() src;
   closeResult: string;
   categorias: {};
@@ -24,12 +28,26 @@ export class AddIndisciplinaComponent implements OnInit {
   public solapin: string;
   public grupo: string;
   public implicados: Array<{ nombre_completo: string; solapin: string; grupo: string }> = [];
+  
+  demandantes: {};
+  sancionados: {};
+
+  solapinDemandante: string = '';
+  cargoDemandante: string = '';
+  selectedDemandante: string;
+
+  currentSancionado:string='';
+  solapinSancionado: string = '';
+  grupoSancionado: string = '';
+  selectedSancionado: string;
 
   constructor(
     private modalService: NgbModal,
     private formBuilder: FormBuilder,
     private myServicio: IndisciplinasService,
     private myServicioCategorias: CategoriasService,
+    private myServicioDemandantes: DemandantesService,
+    private myServicioSancionados: SancionadosService,
     private ruta: Router
   ) {}
 
@@ -52,6 +70,42 @@ export class AddIndisciplinaComponent implements OnInit {
     this.categorias = this.myServicioCategorias.getCategorias().subscribe(data => {
       this.categorias = data; //lleno los categorias desde el servicio
     });
+
+    //cargando los demandates que tenemos en la BD para ayudar con el completamiento
+    this.demandantes = this.myServicioDemandantes.getDemandantes().subscribe(data => {
+      this.demandantes = data; //lleno los demandantes desde el servicio
+    });
+
+    //cargando los demandates que tenemos en la BD para ayudar con el completamiento
+    this.sancionados = this.myServicioSancionados.getSancionados().subscribe(data => {
+      this.sancionados = data; //lleno los sancionados desde el servicio
+    });
+  }
+
+  //en el cambio del datalist, cargo los demás campos del dato seleccionado
+  onDemandanteChanged(demandanteName) {
+    this.selectedDemandante = this.getSelectedDemandanteByName(demandanteName);
+    if (this.selectedDemandante){
+      this.solapinDemandante = this.selectedDemandante['solapin'];
+      this.cargoDemandante = this.selectedDemandante['cargo'];
+    }
+  }
+
+  getSelectedDemandanteByName(selectedName: string): string {
+    return this.demandantes['data'].find(demandante => demandante.nombre_completo === selectedName);
+  }
+
+  //Lo mismo para los sancionados
+  onSancionadoChanged(sancionadoName) {
+    this.selectedSancionado = this.getSelectedSancionadoByName(sancionadoName);
+    if (this.selectedSancionado){
+      this.solapinSancionado = this.selectedSancionado['solapin'];
+      this.grupoSancionado = this.selectedSancionado['grupo'];
+    }
+  }
+
+  getSelectedSancionadoByName(selectedName: string): string {
+    return this.sancionados['data'].find(sancionado => sancionado.nombre_completo === selectedName);
   }
 
   addImplicado() {
@@ -59,11 +113,25 @@ export class AddIndisciplinaComponent implements OnInit {
     const psolapin = this.form.controls.sancionado_solapin.value;
     const pgrupo = this.form.controls.sancionado_grupo.value;
 
-    this.implicados.push({ nombre_completo: pnombre, solapin: psolapin, grupo: pgrupo });
+    //verificando que ya no exista en la lista
+    if (!this.implicados.find(implicado=>implicado.solapin===psolapin))
+    {
+      this.implicados.push({ nombre_completo: pnombre, solapin: psolapin, grupo: pgrupo });
+    }
+    else {
+      this.existeSancionadoSwal.show();
+
+    }
+
+    //limpiando los componentes
+    this.currentSancionado="";
+    this.solapinSancionado="";
+    this.grupoSancionado="";
+
   }
 
   EliminarImplicado(i) {
-    this.implicados.splice(i, 1);    
+    this.implicados.splice(i, 1);
   }
 
   open(content) {
@@ -71,12 +139,11 @@ export class AddIndisciplinaComponent implements OnInit {
   }
 
   onSubmit() {
-    
-    const implicados= {
-       implicados: this.implicados      
-    }
+    const implicados = {
+      implicados: this.implicados
+    };
 
-  const result: any = Object.assign({}, this.form.value, implicados);
+    const result: any = Object.assign({}, this.form.value, implicados);
     //console.log(result);
 
     this.myServicio.addIndisciplina(result).subscribe(
